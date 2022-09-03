d0e6a7661339cdcd00c580f2e5676a5a3d3f9b2d
Saauren Mankad <sman0039@student.monash.edu>
2022-09-03 20:33:09 +1000

    Check login state on render of application

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -3,8 +3,8 @@
 	<div class="panel">
 		<h2 class="text-center">Login</h2>
 		<?= $this->Form->create(); ?>
-			<?= $this->Form->input('username'); ?>
-			<?= $this->Form->input('password', array('type' => 'password')); ?>
+			<?= $this->Form->controller('username'); ?>
+			<?= $this->Form->controller('password', array('type' => 'password')); ?>
 			<?= $this->Form->submit('Login', array('class' => 'button')); ?>
 		<?= $this->Form->end(); ?>
 	</div>
