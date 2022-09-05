16f9b15463079c7bec2c7a6d20db522b033f95ed
Saauren Mankad <sman0039@student.monash.edu>
2022-09-05 17:07:02 +1000

    Fix bug in validator

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -69,12 +69,13 @@ class UsersTable extends Table
         $validator
             ->scalar('role')
             ->requirePresence('role', 'create')
-            ->notEmptyString('role');
-            ->notEmptyString('role');
+
+            ->notEmptyString('role')
             ->add('role', 'inList', [
                 'rule' => ['inList', ['admin', 'regular']],
                 'message' => 'Please enter a valid role'
             ]);
+
 
         $validator
             ->integer('client_id')
