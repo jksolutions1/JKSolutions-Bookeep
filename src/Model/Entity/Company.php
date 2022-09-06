eeecef5decbb101937e0e7b1d35ed332c66dd33a
Saauren Mankad <sman0039@student.monash.edu>
2022-09-06 20:56:48 +1000

    Update company

--- src/Model/Entity/Company.php
+++ src/Model/Entity/Company.php
@@ -11,7 +11,7 @@ use Cake\ORM\Entity;
  * @property int $id
  * @property string $name
  * @property string $address
- * @property int $contactno
+ * @property string $contactno
  * @property int $client_id
  *
  * @property \App\Model\Entity\Client $client
