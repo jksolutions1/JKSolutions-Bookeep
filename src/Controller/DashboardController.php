5db482aea73d84bfd32b46f0f4480532b150f68d
Saauren Mankad <sman0039@student.monash.edu>
2022-09-06 16:45:58 +1000

    Attempt of putting appointments table in dashbboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,12 +3,15 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-
 class DashboardController extends AppController {
 
     public function index()
     {
-      
+        $appointments=$this->loadModel ( 'Appointments' );
+
+
+        $appointments->find('all');
+
     }
 
 }
--- templates/Dashboard/index.php
+++ templates/Dashboard/index.php
@@ -1,82 +1,123 @@
-<div class="container-fluid">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
+			<div class="page-header">
+				<h1>
+					Dashboard
+				</h1>
+			</div>
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<div class="jumbotron">
 						<h2>
-							Hello, world!
+							Adding a new client?
+
 						</h2>
 						<p>
-							This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+							<br>
 						</p>
 						<p>
-							<a class="btn btn-primary btn-large" href="#">Learn more</a>
+							<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'clients', 'action' => 'add']); ?>">Add Client</a>
 						</p>
 					</div>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-6">
 					<div class="jumbotron">
 						<h2>
-							Hello, world!
+							Managing an existing client?
 						</h2>
 						<p>
-							This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+                            <br>
 						</p>
 						<p>
-							<a class="btn btn-primary btn-large" href="#">Learn more</a>
-						</p>
-					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="jumbotron">
-						<h2>
-							Hello, world!
-						</h2>
-						<p>
-							This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
-						</p>
-						<p>
-							<a class="btn btn-primary btn-large" href="#">Learn more</a>
+							<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'clients', 'action' => 'index']); ?>">Manage clients</a>
 						</p>
 					</div>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h3>
+						Upcoming Appointments
+					</h3>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
-		<div class="col-md-4">
-			<h2>
-				Heading
-			</h2>
-			<p>
-				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-			</p>
-			<p>
-				<a class="btn" href="#">View details »</a>
-			</p>
-		</div>
+        <?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Appointment[]|\Cake\Collection\CollectionInterface $appointments
+ */
+
+echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css', ['block' => true]);
+echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js', ['block' => true]);
+echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['block' => true]);
+
+?>
+<div class="appointments index content">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Appointment List</h1>
+        <a href="<?= $this->Url->build(['action' => 'add']) ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> New appointment </a>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('client_name') ?></th>
+                    <th><?= $this->Paginator->sort('company_id') ?></th>
+                    <th><?= $this->Paginator->sort('date') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($appointments as $appointment): ?>
+                <tr>
+                    <td><?= $this->Number->format($appointment->id) ?></td>
+                    <td><?= h($appointment->client->firstname)?> <?= h($appointment->client->lastname)?></td>
+                    <td><?= h($appointment->company->name)?></td>
+                    <td><?= h($appointment->date) ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('View'), ['action' => 'view', $appointment->id]) ?>
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $appointment->id]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $appointment->id], ['confirm' => __('Are you sure you want to delete # {0}?', $appointment->id)]) ?>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+        });
+    </script>
+</div>
+
 	</div>
 </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
