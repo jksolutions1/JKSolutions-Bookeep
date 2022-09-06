511814aef47b7531c33fa29d235598443a2de149
Your Name <you@example.com>
2022-09-06 20:17:36 +1000

    Client&Document Add Page

--- templates/Documents/add.php
+++ templates/Documents/add.php
@@ -10,7 +10,11 @@
     <div class="documents form content">
         <?= $this->Form->create($document, ['type'=>'file']) ?>
             <?php
-                echo $this->Form->control('type');
+                //echo $this->Form->control('type');
+                echo $this->Form->label('Document Type');
+                $options = ['1' => 'Authority for Agency', '2' => 'Client Engagement Agreement', '3' => 'Option3', '4' => 'Option4'];
+                echo $this->Form->select('type', $options, ['empty' => true]);
+        
                 echo $this->Form->control('client_id', ['options' => $clients]);
                 echo $this->Form->control('file',['type'=>'file']);
             ?>
