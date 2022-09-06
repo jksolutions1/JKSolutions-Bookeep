65d2e445fd0c8402ae77af9f67e3d46cfa33bf34
Qihao Hu <qhuu0013@student.monash.edu>
2022-09-06 15:56:16 +1000

    change the view of edit document page

--- templates/Documents/edit.php
+++ templates/Documents/edit.php
@@ -7,12 +7,16 @@
 ?>
 
 <h1 class="h3 mb-2 text-gray-800">Edit Document</h1>
-<?= $this->Form->create($document)?>
-    <?php
-        echo $this->Form->control('type');
-        echo $this->Form->control('client_id', ['options' => $clients]);
-        echo $this->Form->control('file');
-    ?>
-  
-<?= $this->Form->button(__('Submit')) ?>
-<?= $this->Form->end() ?>
+    <div class="documents form content">
+        <?= $this->Form->create($document, ['type'=>'file']) ?>
+        <fieldset>
+            <legend><?= __('Add Document') ?></legend>
+            <?php
+                echo $this->Form->control('type');
+                echo $this->Form->control('client_id', ['options' => $clients]);
+                echo $this->Form->control('file',['type'=>'file']);
+            ?>
+        </fieldset>
+        <?= $this->Form->button(__('Submit')) ?>
+        <?= $this->Form->end() ?>
+    </div>
