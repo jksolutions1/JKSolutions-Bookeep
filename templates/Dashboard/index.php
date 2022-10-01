6dc8505ce43836345b5af02312fe91a939b5c01c
Saauren Mankad <sman0039@student.monash.edu>
2022-10-01 19:38:50 +1000

    Change page name based on user role

--- templates/Dashboard/index.php
+++ templates/Dashboard/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Appointment[]|\Cake\Collection\CollectionInterface $appointments
@@ -11,76 +12,83 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap 4, from LayoutIt!</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="author" content="LayoutIt!">
+	<title>Dashboard</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
 
-  </head>
-  <body>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="page-header">
-				<h1>
-					Dashboard
-				</h1>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<div class="jumbotron">
-						<h2>
-							Adding a new client?
+</head>
 
-						</h2>
-						<p>
-							<br>
-						</p>
-						<p>
-							<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'clients', 'action' => 'add']); ?>">Add Client</a>
-						</p>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h1>
+					<?php if ($isAdmin) {						
+						echo "Admin Dashboard";
+					}
+
+					 else {				
+						echo "Dashboard" ;
+					 }
+					 ?>
+					 </h1>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="jumbotron">
+							<h2>
+								Adding a new client?
+
+							</h2>
+							<p>
+								<br>
+							</p>
+							<p>
+								<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'clients', 'action' => 'add']); ?>">Add Client</a>
+							</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="jumbotron">
+							<h2>
+								Making a new appointment?
+							</h2>
+							<p>
+								<br>
+							</p>
+							<p>
+								<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'appointments', 'action' => 'add']); ?>">Make Appointment</a>
+							</p>
+						</div>
 					</div>
 				</div>
-				<div class="col-md-6">
-					<div class="jumbotron">
-						<h2>
-							Making a new appointment?
-						</h2>
-						<p>
-                            <br>
-						</p>
-						<p>
-							<a class="btn btn-primary btn-large" href="<?= $this->Url->build(['controller' => 'appointments', 'action' => 'add']); ?>">Make Appointment</a>
-						</p>
+				<div class="row">
+					<div class="col-md-12">
+						<h3>
+
+						</h3>
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<h3>
-						
-					</h3>
-				</div>
-			</div>
+
+
+
+
 		</div>
-
-
-
-
 	</div>
-</div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-  </body>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+
 </html>
