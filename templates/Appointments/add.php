4e21cb1978097ecc5e04646db0918abc32fb50c0
Qihao Hu <qhuu0013@student.monash.edu>
2022-10-02 19:31:17 +1100

    change the add page

--- templates/Appointments/add.php
+++ templates/Appointments/add.php
@@ -6,27 +6,30 @@
  * @var \Cake\Collection\CollectionInterface|string[] $companies
  */
 ?>
-
 <h1 class="h3 mb-2 text-gray-800">Add Appointment</h1>
-<?= $this->Form->create($appointment)?>
-    <?php
-        echo $this->Form->control('client_id', ['options' => $clients]);
-        echo $this->Form->control('company_id', ['options' => $companies]);
+    <div class = "row">
+        <div class = "col-lg-4">
+            <?= $this->Form->create($appointment)?>
+                <?php
+                    echo $this->Form->control('client_id', ['options' => $clients]);
+                    echo $this->Form->control('company_id', ['options' => $companies]);
 
-        echo $this->Form->label('Appointment Date');
-        echo $this->Form->input('date', [
-            'type' => 'date',
-            'required',
-            'default' => date('Y-m-d'),
-            'min' => date('Y-m-d')
-        ]);
+                    echo $this->Form->label('Appointment Date');
+                    echo $this->Form->input('date', [
+                        'type' => 'date',
+                        'required',
+                        'default' => date('Y-m-d'),
+                        'min' => date('Y-m-d')
+                    ]);
 
-        echo $this->Form->label('Appointment Place(address)');
-        $options = ['1' => 'Home (Virtual meeting)', '2' => 'At Bookeep company (Face meeting)', '3' => 'Option3', '4' => 'Option4'];
-        echo $this->Form->select('address', $options, ['empty' => true]);
+                    echo $this->Form->label('Appointment Place(address)');
+                    $options = ['1' => 'Home (Virtual meeting)', '2' => 'At Bookeep company (Face meeting)', '3' => 'Option3', '4' => 'Option4'];
+                    echo $this->Form->select('address', $options, ['empty' => true]);
 
-        echo $this->Form->control('appointment_description',array('type'=>'text','maxlength'=>'100','size'=>'8'));
-    ?>
-  
-<?= $this->Form->button(__('Submit')) ?>
-<?= $this->Form->end() ?>
+                    echo $this->Form->control('appointment_description',array('type'=>'text','maxlength'=>'100','size'=>'8'));
+                ?>
+            
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
--- templates/Appointments/edit.php
+++ templates/Appointments/edit.php
@@ -8,27 +8,31 @@
 ?>
 
 <h1 class="h3 mb-2 text-gray-800">Edit Appointment</h1>
-<?= $this->Form->create($appointment)?>
-    <?php
-        echo $this->Form->control('client_id', ['options' => $clients]);
-        echo $this->Form->control('company_id', ['options' => $companies]);
+    <div class = "row">
+        <div class = "col-lg-4">
+            <?= $this->Form->create($appointment)?>
+                <?php
+                    echo $this->Form->control('client_id', ['options' => $clients]);
+                    echo $this->Form->control('company_id', ['options' => $companies]);
 
-        echo $this->Form->label('Appointment Date');
-        echo $this->Form->input('date', [
-            'type' => 'date',
-            'required',
-            'default' => date('Y-m-d'),
-            'min' => date('Y-m-d')
-        ]);
+                    echo $this->Form->label('Appointment Date');
+                    echo $this->Form->input('date', [
+                        'type' => 'date',
+                        'required',
+                        'default' => date('Y-m-d'),
+                        'min' => date('Y-m-d')
+                    ]);
 
-        echo $this->Form->label('Appointment Place(address)');
-        $options = ['1' => 'Home (Virtual meeting)', '2' => 'At Bookeep company (Face meeting)', '3' => 'Option3', '4' => 'Option4'];
-        echo $this->Form->select('address', $options, ['empty' => true]);
+                    echo $this->Form->label('Appointment Place(address)');
+                    $options = ['1' => 'Home (Virtual meeting)', '2' => 'At Bookeep company (Face meeting)', '3' => 'Option3', '4' => 'Option4'];
+                    echo $this->Form->select('address', $options, ['empty' => true]);
 
-        echo $this->Form->control('appointment_description',array('type'=>'text','maxlength'=>'100','size'=>'8'));
+                    echo $this->Form->control('appointment_description',array('type'=>'text','maxlength'=>'100','size'=>'8'));
 
 
-    ?>
-  
-<?= $this->Form->button(__('Submit')) ?>
-<?= $this->Form->end() ?>
+                ?>
+            
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
