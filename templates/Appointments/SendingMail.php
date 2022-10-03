e095538e0bf61ed0f0456abb0b3a78322f59e23f
Qihao Hu <qhuu0013@student.monash.edu>
2022-10-03 23:07:57 +1100

    fixing the bug

--- templates/Appointments/SendingMail.php
+++ templates/Appointments/SendingMail.php
@@ -9,8 +9,8 @@
 <?php
 
 if ($relativeAppointments->date >= $currentTime){
-    foreach ($appointments as $appointment):
-        $to = $relativeAppointments->client->email;//$this->$clients->email;//destination email
+    foreach ($relativeAppointments as $appointment):
+        $to = $appointment->client->email;//$this->$clients->email;//destination email
         $subject ="Appointment Notification";//mail title
         $message ="Hello";//mail content
         $from = "bookeep@u22s2105.monash-ie.me";
