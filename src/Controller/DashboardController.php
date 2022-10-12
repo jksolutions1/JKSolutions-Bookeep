a3651347ed0ace2554f0c05e5e749945008acd67
Saauren Mankad <sman0039@student.monash.edu>
2022-10-12 18:07:21 +1100

    Update DashboardController.php

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -23,13 +23,15 @@ class DashboardController extends AppController
 
         $currentTime = FrozenTime::now();
 
+        // Get all upcoming appointments in the future
         $recentAppointments = $this->fetchTable('Appointments')->find('all', [
             'conditions' => ['Appointments.date >' => $currentTime],
             'contain' => ['Clients', 'Companies']])->all();
 
-
+        // Get all recently added clients
         $recentclients = $this->fetchTable('Clients')->find('all', ['limit' => 3, 'order' => 'Clients.id DESC'])->all();
 
+        // Save variables to be accessed by front end
         $this->set('recentclients', $recentclients);
         $this->set('recentAppointments', $recentAppointments);
         $this->set('current', $currentTime);
